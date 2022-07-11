9c7ef211ab0f0c0b4c733518b73801ba473ddc80
sho9029 <yatabe_9029@yahoo.co.jp>
2022-07-12 00:48:11 +0900

    Cookingクラスの変更

--- Shared/Cooking.cs
+++ Shared/Cooking.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CookingSuggester.Shared;
 
 public class Cooking
 {
+    [Key]
     [Column("id")]
     public int Id { get; set; }
 
     [Column("name")]
     public string Name { get; set; }
 
+    // カンマ区切りで複数個指定
     [Column("material")]
-    public string[] Materials { get; set; }
+    public string Materials { get; set; }
 
+    // 改行は"\n"を使用
     [Column("process")]
     public string Process { get; set; }
 }
