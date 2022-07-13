5e0c1a9b0af8a1a7e0de36065c95955f2a8ac5e5
yatabe sho <yatabe_9029@yahoo.co.jp>
2022-07-13 20:10:57 +0900

    新しいAPIの追加

--- Server/Controllers/SuggestionController.cs
+++ Server/Controllers/SuggestionController.cs
@@ -18,6 +18,25 @@ public class SuggestionController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Cooking>>> Get()
     {
-        return await _context.cookings.ToListAsync();
+        return await _context.cookings.ToArrayAsync();
+    }
+
+    [HttpGet("get/{id}")]
+    public async Task<ActionResult<Cooking>> Get(int id)
+    {
+        return await _context.cookings.SingleAsync(i => i.Id == id);
+    }
+
+    [HttpPost("exclude")]
+    public async Task<ActionResult<IEnumerable<Cooking>>> Post([FromBody] string query)
+    {
+        if (string.IsNullOrEmpty(query))
+        {
+            return await _context.cookings.ToArrayAsync();
+        }
+
+        var ids = query.Split(',').Select(i => int.Parse(i));
+        return await _context.cookings
+            .Where(i => !ids.Contains(i.Id)).ToArrayAsync();
     }
 }
